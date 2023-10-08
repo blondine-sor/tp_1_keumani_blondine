0ad8406c302aebf59f859d8c7179f4ab2c2ceae7
blondine-sor <bkeumani@gmail.com>
2023-10-07 20:30:26 -0400

    Verification du tableau

---
+++ form_Results.php
@@ -0,0 +1,22 @@
+<?php
+require_once("functions.php");
+//utilise le script des fonctions
+
+if($_POST){
+    $password_1 = $_POST["password_1"];
+
+    if(!empty($password_1 )){
+       echo'<h3> Mot de passe : '.$password_1;echo'</h3>';
+       
+    }//Verification du champ du mot de passes
+
+    else{
+        echo'<h3> Veullez Entrer un Mot de passe!</h3>';
+        
+    }//Verification du  champ de texte
+    
+    
+
+}
+?>
+<a href="index.php">Retour</a>
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    
+<form method="post" action="form_Results.php">
+        <fieldset>
+            <legend><h2>Formulaire</h2></legend>
+    <label for="password1">Password</label>
+    <input type ="password" id="motdepasse" name="password_1" value="">
+
+</br>
+    <button type="submit">Valider</button>
+
+        </fieldset>
+    </form>
+</body>
+</html>
